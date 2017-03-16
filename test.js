5e0460abc515aa0239680715dc643acaa85caba8
manuelapilongo <manuel.apilongo@rullion.co.uk>
2017-03-16 10:13:36 +0000

    init commit

---
+++ test.js
@@ -0,0 +1,14 @@
+
+"use strict";
+
+jsDoc_test = {};
+
+/**
+ * Function that initialises a base {Object}
+ * @return {Object} Base object
+ */
+jsDoc_test.init = function () {
+    return {
+        initialized: true,
+    };
+};
