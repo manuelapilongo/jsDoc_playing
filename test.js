292915948ce77f62b6d1a5b503453f1f467996e3
manuelapilongo <manuel.apilongo@rullion.co.uk>
2017-03-16 10:18:49 +0000

    init commit

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ jsDoc_test = {};
 
 /**
  * Function that initialises a base {Object}
+ * [Following jsDoc guide]{@link http://usejsdoc.org/tags-inline-link.html|jsDoc link}
  * @return {Object} Base object
  */
 jsDoc_test.init = function () {
